2d9894db8d05123b071e694c1830ce8d4dd7ebce
saxenaAks <87381713+saxenaAks@users.noreply.github.com>
2023-11-25 14:07:23 +0000

    adding comment

--- main.sql
+++ main.sql
@@ -10,7 +10,13 @@ GROUP BY Month
 ORDER BY Month DESC;
 
 -- Real-time Updates
+-- Example: Stored Procedure for Inserting User
+-- This stored procedure inserts a new user into the 'users' table.
+-- Parameters:
+--   - p_username: The username of the new user.
+--   - p_email: The email address of the new user.
 CREATE PROCEDURE UpdateFinancialData()
+-- starting the procedure 
 BEGIN
     INSERT INTO FinancialData
     SELECT 
@@ -21,8 +27,10 @@ BEGIN
     FROM financial_data
     WHERE transaction_date = CURRENT_DATE();
 END;
+-- End the procedur
 
 -- User Guide
+-- creating table 
 CREATE TABLE UserGuide (
     Section VARCHAR(50),
     Content TEXT
@@ -37,15 +45,17 @@ INSERT INTO UserGuide (Section, Content) VALUES
 ('Security Measures', 'Role-based access control and encryption ensure data security.');
 
 -- Security Measures
+-- create tale amd roleid make primary key 
 CREATE TABLE Roles (
     RoleID INT PRIMARY KEY,
     RoleName VARCHAR(50) UNIQUE
 );
 
+--Here we inserting some values into a table 
 INSERT INTO Roles (RoleID, RoleName) VALUES
 (1, 'Admin'),
 (2, 'Analyst');
-
+--creating table and user id make primary key
 CREATE TABLE UserRoles (
     UserID INT PRIMARY KEY,
     RoleID INT,
@@ -53,12 +63,14 @@ CREATE TABLE UserRoles (
 );
 
 -- Documented SQL Queries
+-- Creating table and make id as primary key
 CREATE TABLE SQLQueries (
     QueryID INT PRIMARY KEY,
     QueryDescription VARCHAR(255),
     Query TEXT
 );
 
+-- here we inserting data with some values  
 INSERT INTO SQLQueries (QueryID, QueryDescription, Query) VALUES
 (1, 'Financial Data Extraction', 'SELECT * FROM FinancialData;'),
 (2, 'Real-time Updates', 'EXECUTE UpdateFinancialData;'),
@@ -66,10 +78,12 @@ INSERT INTO SQLQueries (QueryID, QueryDescription, Query) VALUES
 (4, 'Security Measures', 'SELECT * FROM Roles;');
 
 -- Additional Technologies Used
+-- here we create table and id makes as primary Key
 CREATE TABLE TechnologiesUsed (
     TechnologyID INT PRIMARY KEY,
     TechnologyName VARCHAR(50)
 );
+-- Intserting data into table Technogiesused
 
 INSERT INTO TechnologiesUsed (TechnologyID, TechnologyName) VALUES
 (1, 'SQL'),
